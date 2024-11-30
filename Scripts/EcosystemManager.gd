b287c52c1c8849f2225c3bbbe929a99848e3a3a2
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-11-30 22:54:25 +0000

    Trees now use spawn number instead of probability

--- Scripts/EcosystemManager.gd
+++ Scripts/EcosystemManager.gd
@@ -2,8 +2,11 @@ extends NavigationRegion3D
 
 @export var predator_num : int
 @export var prey_num : int
-@export var tree_threshold : float
-@export_range(0, 1, 0.001) var tree_probability : float
+@export var tree_num : int
+#@export var tree_threshold : float
+#@export_range(0, 1, 0.001) var tree_probability : float
+
+@export var noise_threshold : float
 
 @export var predator_scene : PackedScene
 @export var prey_scene : PackedScene
@@ -16,7 +19,7 @@ extends NavigationRegion3D
 var tree_multimesh : MultiMeshInstance3D
 var leaves_multimesh : MultiMeshInstance3D
 
-var tree_positions : Array[Vector3]
+#var tree_positions : Array[Vector3]
 
 var high_positions : Array[Vector3]
 var low_positions : Array[Vector3]
@@ -53,9 +56,9 @@ func generate_spawn_positions():
 	for x in range(-240, 240):
 		for y in range(-240, 240):
 			var noise : float = fast_noise.get_noise_2d(x, y)
-			if noise > tree_threshold:
+			if noise > noise_threshold:
 				high_positions.append(Vector3(x / 10.0, 0.0, y / 10.0))
-			elif noise < tree_threshold:
+			elif noise < noise_threshold:
 				low_positions.append(Vector3(x / 10.0, 0.0, y / 10.0))
 
 func generate_water():
@@ -71,21 +74,16 @@ func generate_trees():
 	tree_multimesh.multimesh.transform_format = MultiMesh.TRANSFORM_3D
 	tree_multimesh.multimesh.mesh = tree_mesh
 	
-	for x in range(-240, 240):
-		for y in range(-240, 240):
-			if fast_noise.get_noise_2d(x, y) > tree_threshold:
-				if (randf() > (1-tree_probability)):
-					tree_positions.append(Vector3(x / 10.0, 1, y / 10.0))
+	tree_multimesh.multimesh.instance_count = tree_num
+	leaves_multimesh.multimesh.instance_count = tree_num
 	
-	tree_multimesh.multimesh.instance_count = tree_positions.size()
-	leaves_multimesh.multimesh.instance_count = tree_positions.size()
-	
-	for i in tree_positions.size():
-		add_tree(i, tree_positions[i])
+	for i in tree_num:
+		var position_index : int = randi_range(0, low_positions.size() - 1)
+		add_tree(i, low_positions[position_index])
 	
 	add_child(tree_multimesh)
 	add_child(leaves_multimesh)
 
 func add_tree(index : int, tree_pos : Vector3):
-	tree_multimesh.multimesh.set_instance_transform(index, Transform3D(Basis(), tree_pos))
-	leaves_multimesh.multimesh.set_instance_transform(index, Transform3D(Basis(), tree_pos + Vector3(0.0, 1.0, 0.0)))
+	tree_multimesh.multimesh.set_instance_transform(index, Transform3D(Basis(), tree_pos + Vector3.UP))
+	leaves_multimesh.multimesh.set_instance_transform(index, Transform3D(Basis(), tree_pos + Vector3.UP * 2))
