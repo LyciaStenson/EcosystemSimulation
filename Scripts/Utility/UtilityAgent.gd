e240f0d8aadd1bd05297456d33209c0f6a885d0f
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-12-06 23:51:32 +0000

    Working on utility AI system

--- Scripts/Utility/UtilityAgent.gd
+++ Scripts/Utility/UtilityAgent.gd
@@ -1,2 +1,18 @@
 extends CharacterBody3D
 class_name UtilityAgent
+
+@onready var nav_agent : NavigationAgent3D = $NavigationAgent
+
+@onready var sensor : UtilitySensor = $UtilitySensor
+
+var birth_time : float
+
+var world_context : UtilityWorldContext
+
+var actions : Array[UtilityAction]
+
+func _ready():
+	birth_time = Time.get_ticks_msec()
+
+func _process(delta):
+	pass
--- Scripts/Utility/UtilitySensor.gd
+++ Scripts/Utility/UtilitySensor.gd
@@ -1,2 +1,15 @@
 extends Area3D
 class_name UtilitySensor
+
+var targetGroups : Array[String]
+
+var targets : Array[Node3D]
+
+func visibility_entered(body : Node3D):
+	for group in targetGroups:
+		if body.is_in_group("Predator"):
+			targets.append(group)
+
+func visibility_exited(body : Node3D):
+	if targets.has(body):
+		targets.erase(body)
