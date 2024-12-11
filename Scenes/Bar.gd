d9109eb38be2b0ca7b70260cc670e5b9792bc2fd
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-12-11 12:58:44 +0000

    Working on utility actions

---
+++ Scenes/Bar.gd
@@ -0,0 +1,10 @@
+extends MeshInstance3D
+class_name Bar
+
+@onready var shader := get_active_material(0) as ShaderMaterial
+
+func set_value(value : float):
+	shader.set_shader_parameter("value", normalize_to_bar(value))
+
+func normalize_to_bar(value : float) -> float:
+	return (value - 0.5) * 0.8
--- Scripts/PreyAgent.gd
+++ Scripts/PreyAgent.gd
@@ -4,10 +4,9 @@ class_name PreyAgent
 var age_proportion : float = 0.0 # age as a proportion of lifetime
 
 var hydration : float = 1.0
-var dehydration_rate : float = 0.05
+var dehydration_rate : float = 0.02
 
-@onready var hydration_bar : MeshInstance3D = $HydrationBar
-@onready var hydration_bar_shader := hydration_bar.get_active_material(0) as ShaderMaterial
+@onready var hydration_bar : Bar = $HydrationBar
 
 @onready var water_sensor : UtilitySensor = $WaterSensor
 
@@ -26,15 +25,12 @@ func _process(delta):
 	if hydration <= 0.0:
 		queue_free()
 	hydration = clampf(hydration, 0.0, 1.0)
-	hydration_bar_shader.set_shader_parameter("value", normalize_to_bar(hydration))
+	hydration_bar.set_value(hydration)
 	world_context.data["hydration"] = hydration
 	age_proportion = ((Time.get_ticks_msec() - birth_time) * 0.001) / lifetime
 	world_context.data["age"] = age_proportion
 	world_context.data["water_in_sight"] = !water_sensor.targets.is_empty()
 	super(delta)
-
-func normalize_to_bar(value : float) -> float:
-	return (value - 0.5) * 0.8
 
 func wander():
 	print("Wander")
